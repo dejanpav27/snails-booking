6d5c14c6f80b2bffb0acf31b7daa4c8bcf429ae3
dejanpav27 <dejanpav27@gmail.com>
2026-05-14 10:04:39 +0200

    Update utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,7 +1,7 @@
 import { format } from 'date-fns';
 
 export function formatPrice(val) {
-  return `£${Number(val).toFixed(2)}`;
+  return `${Number(val).toFixed(0)} RSD`;
 }
 
 export function toDateString(date) {
